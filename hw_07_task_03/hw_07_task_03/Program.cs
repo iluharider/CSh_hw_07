cd389464671db6d1da88c07d41fc3489024e2d27
Ilia Bondarenko <iluharider2002@gmail.com>
2024-03-20 01:39:37 +0300

    hw_07_task_03

---
+++ hw_07_task_03/hw_07_task_03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string sentence = "Это что же получается: ходишь, ходишь в школу, а потом бац - вторая смена";
+
+        string[] words = Regex.Replace(sentence, @"[^\w\s]", "").Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        var groups = words.GroupBy(word => word.Length)
+                          .OrderByDescending(group => group.Count());
+
+        foreach (var group in groups)
+        {
+            Console.WriteLine($"Длина {group.Key}. Количество {group.Count()}");
+            foreach (var word in group)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
