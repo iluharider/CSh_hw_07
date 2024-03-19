6e4bada12d4196b52da531399a03511baa7b6193
Ilia Bondarenko <iluharider2002@gmail.com>
2024-03-20 01:51:10 +0300

    hw_07_task_04

---
+++ hw_07_task_04/hw_07_task_04/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static Dictionary<String, String> EngToRusDictionary = new Dictionary<String, String>()
+    {
+        {"this","эта"},
+        {"dog","собака"},
+        {"eats","ест"},
+        {"too","слишком"},
+        {"much","много"},
+        {"vegetables","овощей"},
+        {"after","после"},
+        {"lunch","обеда"}
+    };
+
+    static void CreateTranslatedBook(String str, int n)
+    {
+        foreach (
+            var chunk in str.Split()
+            .Select(word => EngToRusDictionary[word.ToLower()]
+            .ToUpper()).Chunk(n)
+            )
+        {
+            foreach (var word in chunk)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+    static void Main()
+    {
+        CreateTranslatedBook("This dog eats too much vegetables after lunch", 3);
+    }
+}
